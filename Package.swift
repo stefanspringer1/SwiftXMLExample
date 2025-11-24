80bd54057739aa5c52df5ca3e72ae4703dcac325
Stefan Springer <stefan.springer@icloud.com>
2025-11-24 17:14:18 +0100

    corrections

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.6.2"),
         .package(url: "https://github.com/stefanspringer1/Pipeline.git", from: "1.0.32"),
-        .package(url: "https://github.com/stefanspringer1/PipelineLogging.git", from: "0.0.6"),
+        .package(url: "https://github.com/stefanspringer1/PipelineLogging.git", from: "0.0.7"),
         .package(url: "https://github.com/stefanspringer1/SwiftXML.git", from: "12.0.5"),
     ],
     targets: [
